9381d3f15cc6ce10a0bd1d482bbbd48e46084c26
UjwalMB <bilagiujwal505@gmail.com>
2026-02-01 22:09:24 +0530

    prove_ui

--- frontend/src/ChatWindow.jsx
+++ frontend/src/ChatWindow.jsx
@@ -1,32 +1,27 @@
 import Chat from "./Chat.jsx";
 import "./ChatWindow.css";
 
-function ChatWindow(){
-    return(
-        <div className="chatwindow">
+function ChatWindow() {
+    return (
+          <div className="chatwindow">
             <nav className="navbar">
-                <span>Gemini</span>
-                <span><i className="fa-solid fa-circle-user"></i></span>
+                <span className="logo">Gemini</span>
+                <i className="icon fa-solid fa-circle-user"></i>
             </nav>
 
-            <div className="greeting">
-                <p><span>Hello User!</span></p>
-                <p>How can I help you today?</p>
-            </div>
+            {/* CENTER AREA */}
+            <div className="center-area">
+                <p className="hello">Hello User!</p>
+                <p className="help">How can I help you today?</p>
 
-            <Chat></Chat>
-
-            <div className="input-box">
-                <div className="search-box">
-                    <input type="text" placeholder="Ask Gemini"></input>
-
-                    <div>
-                        <i className="fa-solid fa-plus"></i>
-                        <i className="fa-solid fa-microphone"></i>
-                        <i className="fa-solid fa-paper-plane"></i>
-                    </div>
+                <div className="input-box">
+                    <input type="text" placeholder="Ask Gemini" />
+                    <i className="icon fa-solid fa-plus"></i>
+                    <i className="icon fa-solid fa-microphone"></i>
+                    <i className="icon fa-solid fa-paper-plane"></i>
                 </div>
             </div>
+
             <p className="bottom-info">
                 Gemini can make mistakes, so double-check it
             </p>
