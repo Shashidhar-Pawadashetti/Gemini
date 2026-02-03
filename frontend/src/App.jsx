8045e70074ff2f804cf995a1d0c00cba1bac8732
Shashidhar Pawadashetti <shashipshetti@gmail.com>
2026-02-03 22:08:57 +0530

    Setup basic Chats displaying functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,23 @@ import "./App.css";
 import Sidebar from "./Sidebar.jsx";
 import ChatWindow from "./ChatWindow.jsx";
 import { MyContext } from "./MyContext.jsx";
+import { useState } from "react";
+import {v1 as uuidv1} from "uuid";
 
 function App() {
-  const providerValues = {}; //Passing Values
+  const [prompt, setPrompt] = useState("");
+  const [reply, setReply] = useState(null);
+  const [currThreadId, setCurrThreadId] = useState(uuidv1());
+  const [prevChats, setPrevChats] = useState([]);
+  const [newChat, setNewChat] = useState(true);
+
+  const providerValues = {
+    prompt, setPrompt,
+    reply, setReply,
+    currThreadId, setCurrThreadId,
+    prevChats, setPrevChats,
+    newChat, setNewChat
+  }; //Passing Values
 
   return (
     <div className="app">
--- frontend/src/Chat.jsx
+++ frontend/src/Chat.jsx
@@ -1,9 +1,57 @@
 import "./Chat.css";
+import { useContext, useState, useEffect } from "react";
+import { MyContext } from "./MyContext.jsx";
+import ReactMarkdown from "react-markdown";
+import rehypeHighlight from "rehype-highlight";
+import "highlight.js/styles/atom-one-dark.css";
 
 function Chat(){
+    const {prevChats, setPrevChats, newChat, setNewChat, reply} =useContext(MyContext);
+    const [latestReply, setLatestReply] = useState(null);
+
+    useEffect(() => {
+        if(!prevChats.length) return;
+
+        const content = reply.split(" ");
+
+        let idx = 0;
+        setLatestReply("");
+        const interval = setInterval(() => {
+            setLatestReply(content.slice(0, idx+1).join(" "));
+            idx++;
+
+            if(idx >= content.length) clearInterval(interval);
+        }, 40);
+
+        return () => clearInterval(interval);
+
+    }, [prevChats, reply])
+
+    if (newChat) return null;
     return(
         <>
-            
+            <div className="chats">
+                {newChat && <h1>Where should we start?</h1>}
+
+                {
+                    prevChats?.slice(0, -1).map((chat, idx) =>
+                        <div className={chat.role === "user" ? "user-div" : "gemini-div"} key={idx}>
+                            {chat.role === "user" ?
+                                <p className="user-message">{chat.content}</p> :
+                                <ReactMarkdown rehypePlugins={[rehypeHighlight]}>{chat.content}</ReactMarkdown>
+                            }
+                        </div>
+                    )
+                }
+
+                {
+                    prevChats.length > 0 && latestReply !== null &&
+                    <div className="gemini-div">
+                        <ReactMarkdown rehypePlugins={[rehypeHighlight]}>{latestReply}</ReactMarkdown>
+                    </div>
+                }
+
+            </div>
         </>
     )
 }
